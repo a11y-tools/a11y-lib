a5ffc653128f269033c02e58ad5023b0694555ec
Nicholas Hoyt <nhoyt@illinois.edu>
2017-11-08 13:36:53 -0600

    Remove import of rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-// import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default {
@@ -8,7 +7,6 @@ export default {
     format: 'es'
   },
   plugins: [
-    // resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     })
