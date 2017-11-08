f4af57515a410dd5389bfa6899dc76b9c40b898a
Nicholas Hoyt <nhoyt@illinois.edu>
2017-11-08 13:35:31 -0600

    Uninstall rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve';
+// import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default {
@@ -8,7 +8,7 @@ export default {
     format: 'es'
   },
   plugins: [
-    resolve(),
+    // resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     })
